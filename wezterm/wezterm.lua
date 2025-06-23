4edaacfaf8a592fef5447ec8c046227a015b3b1a
Kuowrk <koadahoakodah@gmail.com>
2025-06-22 20:37:09 -0400

    Add nushell config; Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require("wezterm")
+
+return {
+  -- Start nushell from zsh
+  default_prog = { "zsh", "-l", "-c", "nu" },
+
+  font = wezterm.font({
+    family = "Agave Nerd Font",
+  }),
+  font_rules = {},
+  font_size = 10,
+
+  default_cursor_style = "BlinkingBlock",
+  animation_fps = 1,
+  cursor_blink_ease_in = "Constant",
+  cursor_blink_ease_out = "Constant",
+  cursor_blink_rate = 500,
+
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = "Catppuccin Mocha",
+
+  keys = {
+  		{
+  			key = "t",
+  			mods = "CTRL|SHIFT",
+  			action = wezterm.action.SpawnCommandInNewTab({
+  				args = { "zsh", "-l", "-c", "nu" },
+  			}),
+  		},
+  	},
+}
